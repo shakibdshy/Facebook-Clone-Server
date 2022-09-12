e3bd759386f2aa5628ea16a86dc6544c74fd6100
Md Habibur Rahman <shakibdshy@gmail.com>
2022-09-12 17:20:33 +0600

    Update JWT_SECRET

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -18,14 +18,14 @@ export const signup = async (req, res, next) => {
 
 export const signin = async (req, res, next) => { 
     try {
-        const user = await User.findOne({ name: req.body.name });
+        const user = await User.findOne({ username: req.body.username });
         if (!user) return next(createError(404, "User not found!"));
 
         const isCorrect = await bcrypt.compare(req.body.password, user.password);
 
         if (!isCorrect) return next(createError(400, "Wrong Credentials!"));
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT);
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
         const { password, ...others } = user._doc;
 
         res
